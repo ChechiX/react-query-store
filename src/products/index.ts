34507c116bb1ef805b2dfb90759f921e3135a594
Julio Anccasi <julio.anccasi.dev@gmail.com>
2024-08-13 02:32:30 -0500

    ProductById

--- src/products/index.ts
+++ src/products/index.ts
@@ -1,9 +1,13 @@
 
-
+export { productsApi } from './api/productsApi';
 
 export { ProductCard } from './components/ProductCard';
 export { ProductList } from './components/ProductList';
 
+export { useProducts } from './hooks/useProducts';
+export { useProduct } from './hooks/useProduct';
+
+export type { Product } from './interfaces/product';
 
 export { StoreLayout } from './layout/StoreLayout';
 
@@ -11,4 +15,7 @@ export { StoreLayout } from './layout/StoreLayout';
 export { CompleteListPage } from './pages/CompleteListPage';
 export { MensPage } from './pages/MensPage';
 export { NewProduct } from './pages/NewProduct';
+export { ProductById } from './pages/ProductById';
 export { WomensPage } from './pages/WomensPage';
+
+export * as productActions from './services/actions';
